61b456c8a4ec9faaf21f9b9cb626169d0d03445e
Piyush Santwani <piyush.santwani@justride.in>
2016-06-18 18:59:17 +0530

    fly production

---
+++ DOHACK2016/flightplan.js
@@ -0,0 +1,61 @@
+var plan = require('flightplan');
+
+var appName = 'node-app';
+var username = 'deploy';
+var startFile = 'server.js';
+
+//var tmpDir = appName+'-' + new Date().getTime();
+var tmpDir = appName;
+
+// configuration
+plan.target('staging', [
+  {
+    host: '139.59.9.43',
+    username: username,
+    password : '*****',
+    agent: process.env.SSH_AUTH_SOCK
+  }
+]);
+
+plan.target('production', [
+  {
+    host: '139.59.9.43',
+    username: username,
+    password : '*****',
+    agent: process.env.SSH_AUTH_SOCK
+  },
+//add in another server if you have more than one
+// {
+//   host: '104.131.93.216',
+//   username: username,
+//   agent: process.env.SSH_AUTH_SOCK
+// }
+]);
+
+// run commands on localhost
+plan.local(function(local) {
+  // uncomment these if you need to run a build on your machine first
+  // local.log('Run build');
+  // local.exec('gulp build');
+
+  local.log('Copy files to remote hosts');
+  //var filesToCopy = local.exec('git ls-files', {silent: true});
+  var filesToCopy = local.exec('find . -type f', {silent: true});
+  // rsync files to all the destination's hosts
+  local.transfer(filesToCopy, '/tmp/' + tmpDir);
+});
+
+// run commands on remote hosts (destinations)
+plan.remote(function(remote) {
+  remote.log('Move folder to root');
+  remote.sudo('cp -R /tmp/' + tmpDir + ' ~', {user: username});
+  remote.rm('-rf /tmp/' + tmpDir);
+
+  // remote.log('Install dependencies');
+  // remote.sudo('npm --production --prefix ~/' + tmpDir + ' install ~/' + tmpDir, {user: username});
+
+  remote.log('Reload application');
+  remote.sudo('ln -snf ~/' + tmpDir + ' ~/'+appName, {user: username});
+  remote.exec('forever stop ~/'+appName+'/'+startFile, {failsafe: true});
+  remote.exec('forever start ~/'+appName+'/'+startFile);
+});
--- build/app.js
+++ build/app.js
@@ -10,15 +10,20 @@ var jetpack = _interopDefault(require('fs-jetpack'));
 var env = jetpack.cwd(__dirname).read('env.json', 'json');
 
 const {Menu, MenuItem} = electron.remote;
+var fs = require('fs');
+var fsExtra = require('fs-extra');
 var frontEndFiles = ["style.css", "client.js", "index.html"];
 var backEndFiles = ["package.json", "server.js"];
+var fileOpen = "";
 
-console.log('Loaded environment variables:', env);
+// console.log('Loaded environment variables:', env);
+
+
 
 var app = electron.remote.app;
 var appDir = jetpack.cwd(app.getAppPath());
 
-console.log('The author of this app is:', appDir.read('package.json', 'json').author);
+console.log('I am ', appDir.read('package.json', 'json').author);
 
 /**SB-ADMIN-2.JS**/
 $(function() {
@@ -74,37 +79,44 @@ var editor = new Vue({
 });
 
 function openFileDialog() {
-  electron.dialog.showOpenDialog(function (filenames) {
-    console.log(filenames);
-    var filename = filenames[0];
-    if (filename) {
-      fs.readFile(filename, "utf8", function (err, data) {
-        if (err) { throw err; }
-        editor.$data.filename = filename;
-        editor.$data.input = data;
-      });
-    }
-  });
+  // dialog.showOpenDialog(function (filenames) {
+  //   console.log(filenames);
+  //   var filename = filenames[0];
+  //   if (filename) {
+  //     fs.readFile(filename, "utf8", function (err, data) {
+  //       if (err) { throw err; }
+  //       editor.$data.filename = filename;
+  //       editor.$data.input = data;
+  //     });
+  //   }
+  // });
 }
 
 function saveFileDialog() {
-  function save(filename) {
-    fs.writeFile(filename, editor.$data.input, function (err) {
+  if(fileOpen !== ""){
+    fs.writeFile(fileOpen, editor.$data.input, function (err) {
+      console.log(editor.$data);
       if (err) { throw err; }
       alert("Saved!");
     });
-  }
+  }  
+  // function save(filename) {
+  //   fs.writeFile(filename, editor.$data.input, function (err) {
+  //     if (err) { throw err; }
+  //     alert("Saved!");
+  //   });
+  // }
 
-  if (editor.$data.filename) {
-    save(editor.$data.filename);
-  } else {
-    electron.dialog.showSaveDialog(function (filename) {
-      if (filename) {
-        save(filename);
-        editor.$data.filename = filename;
-      }
-    });
-  }
+  // if (editor.$data.filename) {
+  //   save(editor.$data.filename);
+  // } else {
+  //   dialog.showSaveDialog(function (filename) {
+  //     if (filename) {
+  //       save(filename);
+  //       editor.$data.filename = filename;
+  //     }
+  //   });
+  // }
 }
 
 
@@ -139,9 +151,6 @@ const menu = Menu.buildFromTemplate(template);
 Menu.setApplicationMenu(menu);
 
 
-var fs = require('fs');
-var fsExtra = require('fs-extra')
-
 /**Get list of files**/
 function readFilesInDir(dirPath){
 	fs.realpath(dirPath, function(err, path) {
@@ -156,10 +165,10 @@ function readFilesInDir(dirPath){
     files.forEach(function(f) {
         console.log('Files: ' + f);
         if(frontEndFiles.indexOf(f.toString()) > -1){
-        	$("#frontend_files > ul").append($('<li>').html("<a href='#'>" + f + "</a>"));        	
+        	$("#frontend_files > ul").append($('<li>').html("<a class = 'projectFile' href='#editor'>" + f + "</a>"));        	
         }
         else if(backEndFiles.indexOf(f.toString()) > -1){
-        	$("#backend_files > ul").append($('<li>').html("<a href='#'>" + f + "</a>"));
+        	$("#backend_files > ul").append($('<li>').html("<a class = 'projectFile' href='#editor'>" + f + "</a>"));
         }
     });
 	});
@@ -168,6 +177,7 @@ function readFilesInDir(dirPath){
 /**Creating New Project**/
 var project_name;
 var folder_name;
+var project_dir;
 $("#newProject").on("click", function(){
 	swal({
       title: "DO HACK 2016", 
@@ -182,6 +192,7 @@ $("#newProject").on("click", function(){
     	var dirname = __dirname.toString();
     	dirname = dirname.replace("/build","");
     	var dir = dirname + "/MyApps/" + folder_name;
+      project_dir = dir;
 		if (!fs.existsSync(dir)){
     		fsExtra.mkdirs(dir,function(err){
     			if (err) return console.error(err);
@@ -211,6 +222,28 @@ $("#newProject").on("click", function(){
     		});    		
 		}
     });  
+});
+
+$(document).click(function(event) {    
+    var ele = $(event.target);
+    var className = ele[0].className;
+    if(className == "projectFile"){
+      var name = $(event.target).text();
+      var file = project_dir + "/" + name;
+      console.log(file);
+      fs.readFile(file, 'utf8', function(err, data) {
+          fileOpen = file;
+          console.log("#codeEditor");
+          // $("#codeEditor").val(data);
+          editor.$data.filename = name;
+          editor.$data.input = data;
+          $("#codeEditor").focus();
+      });
+    }
+});
+
+$("#deployProject").on("click", function(){
+  
 });
 }());
 //# sourceMappingURL=app.js.map
